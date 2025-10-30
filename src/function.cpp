155764442d20168be636f78321b9eb699a30832d
Vadim <35382512+SolidniyGoose@users.noreply.github.com>
2025-10-31 00:13:52 +0300

    Update function.cpp
    added namespace, removed relative includes, letter "Y" verification, adjusted some code style

--- src/function.cpp
+++ src/function.cpp
@@ -1,22 +1,21 @@
-#include "../include/function.h"
+#include <function.hpp>
 
-
-std::string delete_vowels(const std::string input){
-    const char vowels[13] = {'a','e','y','u','i','o','A','E','Y','U','I','O'};
-    std::string new_str{};
-
-    for (char letter : input){
-        bool isVowel = false;
-        for (char vowel : vowels){
-            if (letter == vowel){
-                isVowel = true;
-                break;
+namespace text_processing {
+    std::string delete_vowels(const std::string& input){
+        const char vowels[] = {'a','e','u','i','o','A','E','U','I','O'};
+        std::string new_str;
+        for (char letter : input){
+            bool isVowel = false;
+            for (char vowel : vowels){
+                if (letter == vowel){
+                    isVowel = true;
+                    break;
+                }
+            }
+            if (!isVowel) {
+                new_str += letter;
             }
         }
-        if (isVowel == false) {
-            new_str += letter;
-        }
+        return new_str;
     }
-
-    return new_str;
 }
